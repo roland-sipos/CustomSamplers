1286325b38de39466cab300709bf3038c602d329
cbforeva <cbforeva@gmail.com>
2013-05-28 17:03:45 +0200

    Addig QueryHandler interface and unify the access logic among the handlers.

--- src/main/java/utils/CustomSamplerUtils.java
+++ src/main/java/utils/CustomSamplerUtils.java
@@ -1,9 +1,12 @@
 package utils;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 import org.apache.jmeter.samplers.SampleResult;
 
 public class CustomSamplerUtils {
-
+	
 	public static SampleResult getInitialSampleResult(String className) {
 		SampleResult res = new SampleResult();
 		res.setSampleLabel(className);
@@ -31,4 +34,63 @@ public class CustomSamplerUtils {
 		res.setResponseData(responseStr.getBytes());
 	}
 	
+	
+	public static void doReadWith(QueryHandler queryHandler, BinaryFileInfo binaryInfo, 
+			                      SampleResult res, boolean isCheckRead, boolean isSpecial) {
+		HashMap<String, String> hashes = binaryInfo.getRandomHashesAndIDs();
+		String originalID = hashes.get("originalID");
+		String chunkID = hashes.get("chunkID");
+		
+		try {
+			byte[] result = null;	
+			res.sampleStart();
+			result = queryHandler.readBinary(originalID, chunkID, 
+					hashes.get("original") + "__" + hashes.get("chunk"), isSpecial);
+			
+			if (result == null)
+				finalizeResponse(res, false, "500", "The result is empty!");
+			
+			if (isCheckRead) {
+				String filePath = binaryInfo.getBinaryFilePathList().get(originalID).get(chunkID);
+				byte[] fileContent = binaryInfo.read(filePath);
+				if (!Arrays.equals(result, fileContent))
+					finalizeResponse(res, false, "500", "Read value is not correct!");
+			}
+			
+			finalizeResponse(res, true, "200",
+					"Value read for:" + " B:" + originalID + " C:" + chunkID + " Success!");
+			
+		} catch (CustomSamplersException ex) {
+			//log.error("MongoSampler read attempt failed: " + ex.toString());
+			finalizeResponse(res, false, "500", ex.toString());
+		} finally {
+			res.sampleEnd();
+		}
+	}
+	
+	public static void doWriteWith(QueryHandler queryHandler, BinaryFileInfo binaryInfo, 
+			                       SampleResult res, boolean isAssigned, boolean isSpecial) {
+		if (isAssigned) {
+    		String chunkID = "chunk-" + getThreadID(Thread.currentThread().getName()) + ".bin";
+    		String pathToChunk = binaryInfo.getBinaryFilePathList().get("BIGrbinary-0.bin.chunks").get(chunkID);
+    		byte[] chunkContent = binaryInfo.read(pathToChunk);
+    		HashMap<String, String> hashes = binaryInfo.getHashesForIDs("BIGrbinary-0.bin.chunks", chunkID);
+    		try {
+    			res.sampleStart();
+    			queryHandler.writeBinary("BIGrbinary-0.bin.chunks", chunkID, 
+    					hashes.get("original") + "__" + hashes.get("chunk"), chunkContent, isSpecial);
+    			
+    			finalizeResponse(res, true, "200",
+    					"Value read for:" + " B: BIGrbinary-0.bin.chunks" + " C:" + chunkID + " Success!");
+    			
+			} catch (CustomSamplersException ex) {
+				//log.error("PostgreSampler write attempt failed: " + ex.toString());
+				finalizeResponse(res, false, "500", ex.toString());
+			} finally {
+				res.sampleEnd();
+			}
+		} else {
+			// TODO
+		}
+	}
 }
---
+++ src/main/java/utils/QueryHandler.java
@@ -0,0 +1,11 @@
+package utils;
+
+public interface QueryHandler {
+	
+	public void writeBinary(String binaryID, String chunkID, String hash, 
+			                byte[] fileContent, boolean isSpecial) throws CustomSamplersException;
+	
+	public byte[] readBinary(String binaryID, String chunkID, String hash, boolean isSpecial)
+			throws CustomSamplersException;
+	
+}
