c6a0c81470edcd61d9d9cd92cfdc9a29556b0608
cbforeva <cbforeva@gmail.com>
2013-09-18 15:27:00 +0200

    Huge cleanup in BinaryFileInfo: - Removed unnecessary workarounds, - simplified inner structures, - overhaul in public access.

--- src/main/java/utils/BinaryFileInfo.java
+++ src/main/java/utils/BinaryFileInfo.java
@@ -16,204 +16,180 @@ import java.util.TreeMap;
 public class BinaryFileInfo {
 
 	private static BinaryFileInfo instance = null;
-	
+
 	private static String location;
 	private static int numOfFiles;
-	// TreeMap<"binaryId", "metaFilePathList">
-	private static TreeMap<String, String> metaFileList;
-	// TreeMap<"binaryId", TreeMap<"chunkId", "hash"> >
+
 	private static TreeMap<String, HashMap<String, String> > metaInfo;
-    // TreeMap<"binaryId", TreeMap<"chunkId", "path"> >
-	private static TreeMap<String, TreeMap<String, String> > binaryFilePathList;
-	// TreeMap<"binaryId", "path">
-	private static TreeMap<String, String> originalFilePathList;
-	
-    public String getInputLocation() {
-        return location;
-    }
-    public int getNumOfFiles() {
-    	return numOfFiles;
-    }
-    public TreeMap<String, HashMap<String, String> > getMetaInfo() {
-        return metaInfo;
-    }
-    public TreeMap<String, String> getMetaFileList() {
-        return metaFileList;
-    }
-    public TreeMap<String, TreeMap<String, String> > getBinaryFilePathList() {
-        return binaryFilePathList;
-    }
-    public TreeMap<String, String> getOriginalFilePathList() {
-    	return originalFilePathList;
-    }
-    
-    public static BinaryFileInfo getInstance(String location) {
-    	if (instance == null) {
-    		instance = new BinaryFileInfo(location);
-    	}
-    	return instance;
-    }
-    
-    protected BinaryFileInfo(String loc) {
-        location = loc; //baseDir.concat("BIGrbinary-"+ID+".bin.chunks/");
-        metaInfo = new TreeMap<String, HashMap<String, String> >();
-        metaFileList = new TreeMap<String, String>();
-        binaryFilePathList = new TreeMap<String, TreeMap<String, String> >();
-        originalFilePathList = new TreeMap<String, String>();
-        numOfFiles = 0;
-        
-        File[] locFolder = new File(location).listFiles();
-        for (File sub : locFolder) {
-        	if (sub.isDirectory()) {
-        		try {
-        			numOfFiles++;
-        			prepareMETA(sub);
-        			String metaFilePath = getMetaFilePath(sub);
-        			metaFileList.put(sub.getName(), metaFilePath);
-        			TreeMap<String, String> binaryFiles = getBinaryFiles(sub);
-        			binaryFilePathList.put(sub.getName(), binaryFiles);
-        			
-        		} catch (Exception ex) {
-        			System.out.println(ex.toString());
-        		}
-        	} else if (sub.isFile()) { // It's an original, BIG file!
-        		originalFilePathList.put(sub.getName(), sub.getAbsolutePath());
-        	} // else: not usable
-        }
-    }
-    
-    public String getBinaryFileAbsolutePath(String fileName) {
-        return binaryFilePathList.get(fileName).toString();
-    }
-    
-    private void prepareMETA(File folder) throws FileNotFoundException, IOException {
-        String binaryName = folder.getName();
-        File metaFile = new File(getMetaFilePath(folder));
-        BufferedReader in = new BufferedReader(new FileReader(metaFile));
-        String line = "";
-        HashMap<String, String> metaMapForBinary = new HashMap<String, String>();
-        while ((line = in.readLine()) != null) {
-            String cols[] = line.split(" ");
-            metaMapForBinary.put(cols[0], cols[1]);
-        }
-        metaMapForBinary.put("full_id", binaryName);
-        metaInfo.put(binaryName, metaMapForBinary);
-        in.close();
-    }
-    
-    private String getMetaFilePath(File folder) {
-        File[] metaFiles = folder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".info");
-            }
-        });
-        return metaFiles[0].getAbsolutePath();
-    }
-    private TreeMap<String, String> getBinaryFiles(File folder) {
-        File[] files = folder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.toLowerCase().endsWith(".bin");
-            }
-        });
-    	TreeMap<String, String> filePathList = new TreeMap<String, String>();
-        for (int i = 0; i < files.length; ++i) {
-        	filePathList.put(files[i].getName(), files[i].getAbsolutePath());
-        }
-        return filePathList;
-    }
- 
-    public HashMap<String, String> getHashesForIDs(String binaryID, String chunkID) {
-    	HashMap<String, String> retMap = new HashMap<String, String>();
-    	retMap.put("original", getMetaInfo().get(binaryID).get("ORIGINAL"));
-    	retMap.put("chunk", getMetaInfo().get(binaryID).get(chunkID));
-    	return retMap;
-    }
-    
-    public HashMap<String, String> getRandomHashesAndIDs() {
-    	Random random = new Random();
-    	HashMap<String, String> retMap = new HashMap<String, String>();
-    	// Get a random Binary ID.
-		Object[] binaryIDsArray = getMetaFileList().keySet().toArray();
-		String binaryID = (String) binaryIDsArray[random.nextInt(binaryIDsArray.length)];  
-		
-		// Get a random ChunkID from the Binary.
-		Object[] chunkIDsArray = getMetaInfo().get(binaryID).keySet().toArray();
-		String chunkID = (String) chunkIDsArray[random.nextInt(chunkIDsArray.length - 2 + 1) + 2]; // random between range 2:MIN (first two entry is ORIGINAL AND HASH)
-		retMap.put("originalID", binaryID);
-		retMap.put("chunkID", chunkID);
-		
-		// Get the hashes for both the original binary, and it's sub-chunk.
-		String originalHash = getMetaInfo().get(binaryID).get("ORIGINAL");
-		String chunkHash = getMetaInfo().get(binaryID).get(chunkID);
-		
-		retMap.put("original", originalHash);
-		retMap.put("chunk", chunkHash);
-		return retMap;
+	private static TreeMap<String, String> filePathList;
+	private static TreeMap<String, TreeMap<String, String> > chunkPathList;
+
+	public String getInputLocation() {
+		return location;
 	}
-    
-    /** Read the given binary file, and return its contents as a byte array.*/ 
-    public byte[] read(String aInputFileName) {
-        //System.out.println("Reading in binary file named : " + aInputFileName);
-        File file = new File(aInputFileName);
-        //System.out.println("File size: " + file.length());
-        byte[] result = new byte[(int)file.length()];
-        try {
-            InputStream input = null;
-            try {
-                int totalBytesRead = 0;
-                input = new BufferedInputStream(new FileInputStream(file));
-                while(totalBytesRead < result.length){
-                    int bytesRemaining = result.length - totalBytesRead;
-                    //input.read() returns -1, 0, or more :
-                    int bytesRead = input.read(result, totalBytesRead, bytesRemaining); 
-                    if (bytesRead > 0){
-                        totalBytesRead = totalBytesRead + bytesRead;
-                    }
-                }
-                /*
-                    the above style is a bit tricky: it places bytes into the 'result' array; 
-                    'result' is an output parameter;
-                    the while loop usually has a single iteration only.
-                */
-                //System.out.println("Num bytes read: " + totalBytesRead);
-            }
-            finally {
-                //System.out.println("Closing input stream.");
-                input.close();
-            }
-        }
-        catch (FileNotFoundException ex) {
-            System.out.println("File not found.");
-        }
-        catch (IOException ex) {
-            System.out.println(ex.toString());
-        }
-        return result;
-    }
-    
+
+	public int getNumOfFiles() {
+		return numOfFiles;
+	}
+
+	public TreeMap<String, HashMap<String, String> > getMetaInfo() {
+		return metaInfo;
+	}
+
+	public TreeMap<String, String> getFilePathList() {
+		return filePathList;
+	}
+
+	public TreeMap<String, TreeMap<String, String> > getChunkPathList() {
+		return chunkPathList;
+	}
+
+	public String getAbsolutePathFor(String fileName) {
+		return filePathList.get(fileName);
+	}
+
+	public String getAbsolutePathForChunk(String fileName, String chunkName) {
+		return chunkPathList.get(fileName).get(chunkName);
+	}
+
+	public String getPathForStreamerInfo(String fileName) {
+		return filePathList.get(fileName) + ".chunks/STREAMER_INFO.bin";
+	}
+
+	public static BinaryFileInfo getInstance(String location) {
+		if (instance == null) {
+			instance = new BinaryFileInfo(location);
+		}
+		return instance;
+	}
+
+	protected BinaryFileInfo(String loc) {
+		location = loc; //baseDir.concat("BIGrbinary-"+ID+".bin.chunks/");
+		numOfFiles = 0;
+		filePathList = new TreeMap<String, String>();
+		chunkPathList = new TreeMap<String, TreeMap<String, String> >();
+		metaInfo = new TreeMap<String, HashMap<String, String> >();
+
+		File[] locFolder = new File(location).listFiles();
+		for (File sub : locFolder) {
+			if (sub.isFile()) {
+				numOfFiles++;
+				// Store the path in filePathList
+				String binaryName = sub.getName();
+				filePathList.put(sub.getName(), sub.getAbsolutePath());
+				//There must be a sub-dir for the file with .chunks suffix:
+				File dir = new File(sub.getAbsolutePath() + ".chunks");
+				if (dir.isDirectory()) {
+					try {
+						readMetaForFile(binaryName, dir);
+						readChunkPathList(binaryName, dir);
+					} catch (Exception ex) {
+						System.out.println(ex.toString());
+					}
+				}
+			} else {
+				// IGNORE ANYTHING ELSE, dirs already processed.
+			}
+		}
+	}
+
+	/*private String getMetaFilePath(File folder) {
+		File[] metaFiles = folder.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".info");
+			}
+		});
+		return metaFiles[0].getAbsolutePath();
+	}*/ // Clerver mode to get any .info file, but simply use a strict form is faster.
+	private void readMetaForFile(String binaryName, File dir) throws FileNotFoundException, IOException {
+		File metaFile = new File(dir.getAbsolutePath() + "/META-" + binaryName + ".info");
+		BufferedReader in = new BufferedReader(new FileReader(metaFile));
+		String line = "";
+		HashMap<String, String> metaMapForBinary = new HashMap<String, String>();
+		while ((line = in.readLine()) != null) {
+			String cols[] = line.split(" ");
+			metaMapForBinary.put(cols[0], cols[1]);
+		}
+		metaMapForBinary.put("id", binaryName);
+		metaInfo.put(binaryName, metaMapForBinary);
+		in.close();
+	}
+
+	private File[] getChunkFiles(File folder) {
+		File[] chunkFiles = folder.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".bin") && !name.startsWith("STREAMER_INFO");
+			}
+		});
+		return chunkFiles;
+	}
+	private void readChunkPathList(String binaryName, File dir) {
+		TreeMap<String, String> list = new TreeMap<String, String>();
+		File[] chunks = getChunkFiles(dir);
+		for (int i = 0; i < chunks.length; ++i) {
+			list.put(chunks[i].getName(), chunks[i].getAbsolutePath());
+		}
+		chunkPathList.put(binaryName, list);
+	}
+
+
 	public HashMap<String, String> getRandomMeta() {
+		// TODO: Create the random generation customizable.
 		Random random = new Random();
-    	// Get a random Binary ID.
-		Object[] binaryIDsArray = getMetaFileList().keySet().toArray();
-		String binaryID = (String) binaryIDsArray[random.nextInt(binaryIDsArray.length)];  
-		
-		// TODO: Create a RANDOM ACCESS PATTERN with the usage of the IOV and TAG tables.
+		// Get a random Binary ID.
+		Object[] fileNameArray = getFilePathList().keySet().toArray();
+		String binaryID = (String) fileNameArray[random.nextInt(fileNameArray.length)];
 		return getMetaInfo().get(binaryID);
 	}
-	
-	public String getXthBinaryID(int x) {
-		Object[] binaryIDsArray = getOriginalFilePathList().keySet().toArray();
-		return (String) binaryIDsArray[x-1];
-		
-		//System.out.println(getOriginalFilePathList().get("rbinary-HUGE-1.bin"));
-	}
-	
-	public HashMap<String, String> getAssignedMeta() {
-		//TODO: Get an assigned meta information based on current Thread group and id.
+
+	public HashMap<String, String> getAssignedMeta(int type) {
+		//TODO: Get an assigned meta information based on input file or thread group
 		return null;
 	}
-	
-    
+
+	public String getXthFileName(int x) {
+		Object[] fileNameArray = getFilePathList().keySet().toArray();
+		return (String) fileNameArray[x-1];
+	}
+
+	/** Read the given binary file, and return its contents as a byte array.*/ 
+	public byte[] read(String aInputFileName) {
+		//System.out.println("Reading in binary file named : " + aInputFileName);
+		File file = new File(aInputFileName);
+		//System.out.println("File size: " + file.length());
+		byte[] result = new byte[(int)file.length()];
+		try {
+			InputStream input = null;
+			try {
+				int totalBytesRead = 0;
+				input = new BufferedInputStream(new FileInputStream(file));
+				while(totalBytesRead < result.length){
+					int bytesRemaining = result.length - totalBytesRead;
+					//input.read() returns -1, 0, or more :
+					int bytesRead = input.read(result, totalBytesRead, bytesRemaining); 
+					if (bytesRead > 0){
+						totalBytesRead = totalBytesRead + bytesRead;
+					}
+				}
+				/*
+				 * the above style is a bit tricky: it places bytes into the 'result' array; 
+				 * 'result' is an output parameter;
+				 * the while loop usually has a single iteration only.
+				 */
+			}
+			finally {
+				input.close();
+			}
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("File not found.");
+		}
+		catch (IOException ex) {
+			System.out.println(ex.toString());
+		}
+		return result;
+	}
+
 }
