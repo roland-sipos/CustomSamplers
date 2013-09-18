7acee411ef873ef432b32c55322901a76392ed7b
cbforeva <cbforeva@gmail.com>
2013-09-18 15:29:53 +0200

    CosmetiX

--- src/main/java/utils/CustomSamplersException.java
+++ src/main/java/utils/CustomSamplersException.java
@@ -5,7 +5,7 @@ public class CustomSamplersException extends Exception {
 	private static final long serialVersionUID = 357716820999255104L;
 
 	public CustomSamplersException(String etc_is_null) {
-        super(etc_is_null);
-    }
-	
+		super(etc_is_null);
+	}
+
 }
