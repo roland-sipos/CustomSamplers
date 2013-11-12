c4a2819c54abaa66c7073c8fa8c4158aa4e3b00b
CBforeva <cbforeva@gmail.com>
2013-11-12 16:51:35 +0100

    Remove Drizzle. Relocated the Deployer under MySQL.

--- src/main/java/drizzle/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains the CustomSampler of the Drizzle database.
- * 
- * @author rsipos
- */
-package drizzle;
--- src/main/java/mysql/DrizzleDeployer.java
+++ src/main/java/mysql/DrizzleDeployer.java
@@ -1,4 +1,4 @@
-package drizzle;
+package mysql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
