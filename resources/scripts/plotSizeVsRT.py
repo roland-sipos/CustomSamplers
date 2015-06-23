48a234fb04480730b2568ea6855d3e07ac4f123a
CBforeva <cbforeva@gmail.com>
2015-06-23 16:13:52 +0200

    Utility script + testdata, and removed png.

--- resources/scripts/plotSizeVsRT.py
+++ resources/scripts/plotSizeVsRT.py
@@ -1,20 +1,18 @@
 import numpy
 from matplotlib import pyplot as plt
 
-data=numpy.genfromtxt('test.csv',skiprows=1,delimiter=',')
+data=numpy.genfromtxt('test.csv', skiprows=1, delimiter=',')
 
 timestamps = data[:,0]
 latency = data[:,1]
 size = data[:,8]
+size[:] = [(s/1024.0/1024.0) for s in size]
 
-#bins = numpy.linspace(-10, 10, 100)
-
-#plt.plot(size,latency,'*',linewidth=0)
-#plt.hist(size, bins, alpha=0.5)
-plt.hist(size, 100, alpha=0.5)
+plt.plot(size, latency, '*')
 plt.title('RT vs Size')
-plt.xlabel('size [Byte]')
-plt.ylabel('db')
+plt.xlabel('size [MB]')
+plt.ylabel('response time [ms]')
+plt.ticklabel_format(style='plain')
 
 plt.savefig('testPlot.png',dpi=100)
 
